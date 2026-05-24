73f04e9275bb186085084ebee1a74ce3dc66b72a
Shubham Gotad <gotadshubhamu@gmail.com>
2026-05-24 13:13:38 +0530

    fixed bug

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import pickle
 import numpy as np
 import pandas as pd
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the trained model and preprocessing objects
 MODEL_PATH = 'house_model.pkl'
